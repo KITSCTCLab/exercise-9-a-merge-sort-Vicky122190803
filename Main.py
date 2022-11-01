1a99adcddcc7357106c1a66d683fda1258191c33
Vicky122190803 <110010934+Vicky122190803@users.noreply.github.com>
2022-11-01 17:45:16 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,3 +1,33 @@
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@Vicky122190803 
+KITSCTCLab
+/
+exercise-9-a-merge-sort-Vicky122190803
+Public
+generated from brightvarghese/22_ODD_DS_Exercise-9.a
+Code
+Issues
+Pull requests
+1
+Actions
+Projects
+Wiki
+Security
+Insights
+exercise-9-a-merge-sort-Vicky122190803/Main.py /
+@github-classroom
+github-classroom Initial commit
+Latest commit d99e857 3 minutes ago
+ History
+ 1 contributor
+16 lines (13 sloc)  322 Bytes
+
 from typing import List
 
 def merge_sort(data) -> None:
@@ -14,3 +44,18 @@ for item in input_data.split(', '):
     data.append(int(item))
 merge_sort(data)
 print(data)
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+exercise-9-a-merge-sort-Vicky122190803/Main.py at main · KITSCTCLab/exercise-9-a-merge-sort-Vicky122190803
